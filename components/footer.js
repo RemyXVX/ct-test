bf26fe2bff5c00241dafab92ffcb7ef955822a35
RemyXVX <remyfranciscoflores@gmail.com>
2023-07-30 14:31:23 -0700

    Made footer, did some style adjustment

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,23 @@
 const footer = () => {
+  const footerContainer = document.createElement('div');
+  footerContainer.className = 'bg-slate-600 flex items-center';
 
+  const logo = document.createElement('img');
+  logo.src = './logo-dark.png';
+  logo.className = 'ml-8';
+
+  const crText = document.createElement('p');
+  crText.textContent = 'COPYRIGHT 2016. ALL RIGHTS RESERVED.';
+  crText.className = 'text-slate-500 italic text-xs mr-8 font-bold';
+
+  const space = document.createElement('div');
+  space.className = 'flex-1';
+
+  footerContainer.appendChild(logo);
+  footerContainer.appendChild(space);
+  footerContainer.appendChild(crText);
+
+  return footerContainer;
 }
 
 export default footer;
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import './global.css'
 import header from './components/header';
 import main from './components/main'
+import footer from './components/footer'
 
 const indexContainer = document.getElementById('app');
 const headerContent = header();
 const mainContent = main();
+const footerContent = footer();
 
 indexContainer.appendChild(headerContent);
 indexContainer.appendChild(mainContent);
+indexContainer.appendChild(footerContent);
