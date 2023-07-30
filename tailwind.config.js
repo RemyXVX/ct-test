efe737ca850be8bed0797a89b9eb14f95b608290
RemyXVX <remyfranciscoflores@gmail.com>
2023-07-30 13:15:17 -0700

    initial commit, file structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
