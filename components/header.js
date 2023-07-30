976b1c3f0657d655368a8930093f1526e8e3d05e
RemyXVX <remyfranciscoflores@gmail.com>
2023-07-30 13:44:12 -0700

    made file structure, updated indexjs to render my header

---
+++ components/header.js
@@ -0,0 +1,24 @@
+const header = () => {
+  const headerContainer = document.createElement('div')
+  headerContainer.className = 'flex items-center justify-center flex-col';
+
+  const wrapContent = document.createElement('div');
+  wrapContent.className = 'flex items-center space-x-4';
+
+  const imageElement = document.createElement('img');
+  imageElement.src = './hist2.png';
+  imageElement.className = 'w-64';
+
+  const textElement = document.createElement('p');
+  textElement.textContent =
+  'Mount climbs encapsulate the essence of human exploration, resilience, and connection with nature. From the pursuit of personal achievement to the awe-inspiring beauty of untouched landscapes, mountaineering offers an experience unlike any other. As we continue to explore these majestic peaks, let us remember to approach them with respect, responsibility, and a deep appreciation for the wonders of our planets';
+  textElement.className = 'text-center';
+
+  wrapContent.appendChild(imageElement);
+  wrapContent.appendChild(textElement);
+  headerContainer.appendChild(wrapContent);
+
+  return headerContainer;
+}
+
+export default header;
