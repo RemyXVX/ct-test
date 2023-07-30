a56c50b238d022bef12e58208c595df0730f5af0
RemyXVX <remyfranciscoflores@gmail.com>
2023-07-30 15:18:00 -0700

    Made header and footer more responsive

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,13 @@
 const footer = () => {
   const footerContainer = document.createElement('div');
-  footerContainer.className = 'bg-slate-600 flex items-center';
-
+  footerContainer.className = 'bg-slate-600 flex items-center py-2 md:py-4 md:px-24';
   const logo = document.createElement('img');
   logo.src = './logo-dark.png';
-  logo.className = 'ml-8';
+  logo.className = 'ml-4 md:ml-8';
 
   const crText = document.createElement('p');
   crText.textContent = 'COPYRIGHT 2016. ALL RIGHTS RESERVED.';
-  crText.className = 'text-slate-500 italic text-xs mr-8 font-bold';
+  crText.className = 'mr-4 md:mr-8 text-slate-500 italic text-xs font-bold'; // Adjust font size and styles
 
   const space = document.createElement('div');
   space.className = 'flex-1';
--- components/header.js
+++ components/header.js
@@ -1,21 +1,31 @@
 const header = () => {
-  const headerContainer = document.createElement('div')
-  headerContainer.className = 'flex items-center justify-center flex-col pb-6';
+  const headerContainer = document.createElement('div');
+  headerContainer.className = 'flex items-center justify-center flex-col pb-6 md:mx-24';
 
   const wrapContent = document.createElement('div');
-  wrapContent.className = 'flex items-center space-x-4';
+  wrapContent.className = 'flex items-center space-y-4 md:space-y-0 md:space-x-4';
 
   const imageElement = document.createElement('img');
   imageElement.src = './hist2.png';
-  imageElement.className = 'w-64';
+  imageElement.className = 'w-32 md:w-64';
 
   const textElement = document.createElement('p');
   textElement.textContent =
-  'Mount climbs encapsulate the essence of human exploration, resilience, and connection with nature. From the pursuit of personal achievement to the awe-inspiring beauty of untouched landscapes, mountaineering offers an experience unlike any other. As we continue to explore these majestic peaks, let us remember to approach them with respect, responsibility, and a deep appreciation for the wonders of our planets';
-  textElement.className = 'text-center';
+    'Mount climbs encapsulate the essence of human exploration, resilience, and connection with nature. From the pursuit of personal achievement to the awe-inspiring beauty of untouched landscapes, mountaineering offers an experience unlike any other. As we continue to explore these majestic peaks, let us remember to approach them with respect, responsibility, and a deep appreciation for the wonders of our planets';
+  textElement.className = 'text-center md:text-xs text-sm'; 
+  
+  if (window.innerWidth <= 890) {
+    const scrollBox = document.createElement('div');
+    scrollBox.className = 'md:text-xl max-h-32 overflow-y-scroll';
 
-  wrapContent.appendChild(imageElement);
-  wrapContent.appendChild(textElement);
+    scrollBox.appendChild(textElement);
+    wrapContent.appendChild(imageElement);
+    wrapContent.appendChild(scrollBox);
+  } else {
+    wrapContent.appendChild(imageElement);
+    wrapContent.appendChild(textElement);
+  }
+
   headerContainer.appendChild(wrapContent);
 
   return headerContainer;
